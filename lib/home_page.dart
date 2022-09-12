f08be6a099274c65d70b0f611661f268324f04a7
mgudalewicz <michal.gudalewicz@gmail.com>
2022-09-12 21:34:50 +0200

    add BottomNavigationBarItem

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,18 +1,44 @@
-
-
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Invoice',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(),
-    );
+        title: 'Invoice',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: (Text(currentIndex == 0 ? 'Dodawanie faktury' : 'Twoje faktury')),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: currentIndex,
+            onTap: (newIndex) {
+              setState(() {
+                currentIndex = newIndex;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.post_add),
+                label: 'Dodaj fakturę',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.folder),
+                label: 'Wsystkie faktury',
+              ),
+            ],
+          ),
+        ));
   }
 }
