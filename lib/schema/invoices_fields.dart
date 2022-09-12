87d82de8cda90cc4dea6c03ba5d6b2c31b68a573
mgudalewicz <michal.gudalewicz@gmail.com>
2022-09-12 20:05:02 +0200

    Add schema

---
+++ lib/schema/invoices_fields.dart
@@ -0,0 +1,19 @@
+class InvoicesFields {
+  /// Type [String]
+  static const String id = 'id';
+
+  /// Type [String]
+  static const String invoiceNumber = 'invoice_number';
+
+  /// Type [String]
+  static const String counterpartyName = 'counterparty_name';
+
+  /// Type [double]
+  static const String netAmount = 'net_amount';
+
+  /// Type [double]
+  static const String vat = 'vat';
+
+  /// Type [double]
+  static const String grossAmount = 'gross_amount';
+}
