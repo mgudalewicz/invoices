39e5481cbb7b359e631986fd0a2522844776e7a1
mgudalewicz <michal.gudalewicz@gmail.com>
2022-09-12 21:05:55 +0200

    Initiate service locator

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:invoices/home_page.dart';
 import 'package:invoices/firebase_options.dart';
+import 'package:invoices/service_locator.dart';
 
-void main() async{
-    WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
+  configureDepenedencies();
   runApp(const HomePage());
 }
-
