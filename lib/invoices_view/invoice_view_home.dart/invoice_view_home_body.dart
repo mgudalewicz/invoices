e39d7399a934b3e6243d3c7ad5fc612e1eff75e5
mgudalewicz <michal.gudalewicz@gmail.com>
2022-09-17 14:48:28 +0200

    Add a field to search for invoices

--- lib/invoices_view/invoice_view_home.dart/invoice_view_home_body.dart
+++ lib/invoices_view/invoice_view_home.dart/invoice_view_home_body.dart
@@ -1,6 +1,4 @@
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:invoices/invoices_view/invoice_view_details.dart/invoice_view_details_screen.dart';
 import 'package:invoices/invoices_view/invoice_view_home.dart/cubit/invoice_view_home_cubit.dart';
 import 'package:invoices/models/invoice/invoice.dart';
 import 'package:invoices/widgets/app_text_form_field.dart';
@@ -19,7 +17,6 @@ class InvoiceViewHomeBody extends StatefulWidget {
 
 class _InvoiceViewHomeBodyState extends State<InvoiceViewHomeBody> {
   late List<Invoice> invoiceOverviewList = widget.state.invoices;
-  PlatformFile? pickedPdf;
 
   @override
   Widget build(BuildContext context) {
@@ -44,63 +41,99 @@ class _InvoiceViewHomeBodyState extends State<InvoiceViewHomeBody> {
         children: [
           for (final invoice in invoices) ...[
             GestureDetector(
-              onTap: (() => Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => InvoiceViewDetailsScreen(
-                        id: invoice.id,
-                      ),
-                    ),
-                  )),
+              onTap: (() => _dialogBuilder(context, invoice)),
               child: Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: Container(
                     decoration: BoxDecoration(
-                      color: Colors.cyan.shade50,
-                      border: Border.all(color: Colors.cyan.shade500),
+                      color: const Color.fromARGB(255, 205, 223, 232),
+                      border: Border.all(color: Colors.blue),
                       borderRadius: const BorderRadius.all(Radius.circular(5.0)),
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Column(children: [
-                            Row(
-                              children: [
-                                const Text('Numer faktury:'),
-                                const SizedBox(width: 16),
-                                Flexible(
-                                  child: Text(
-                                    invoice.invoiceNumber,
-                                    style: const TextStyle(fontSize: 20.0),
-                                    maxLines: 1,
-                                  ),
-                                ),
-                              ],
+                    child: Column(children: [
+                      Row(
+                        children: [
+                          const Text('Numer faktury:'),
+                          const SizedBox(width: 16),
+                          Flexible(
+                            child: Text(
+                              invoice.invoiceNumber,
+                              style: const TextStyle(fontSize: 20.0),
+                              maxLines: 1,
                             ),
-                            Row(
-                              children: [
-                                const Text('Kontrahent:'),
-                                const SizedBox(width: 36),
-                                Flexible(
-                                  child: Text(
-                                    invoice.counterpartyName,
-                                    style: const TextStyle(fontSize: 20.0),
-                                    maxLines: 1,
-                                  ),
-                                ),
-                              ],
-                            )
-                          ]),
-                        ),
-                        if (invoice.invoicePDF != null && invoice.invoicePDF != '') const Icon(Icons.article),
-                        const SizedBox(width: 10)
-                      ],
-                    ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Text('Kontrahent:'),
+                          const SizedBox(width: 36),
+                          Flexible(
+                            child: Text(
+                              invoice.counterpartyName,
+                              style: const TextStyle(fontSize: 20.0),
+                              maxLines: 1,
+                            ),
+                          ),
+                        ],
+                      )
+                    ]),
                   )),
             ),
           ],
         ],
       ),
+    );
+  }
+
+  Future<void> _dialogBuilder(BuildContext context, Invoice invoice) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          insetPadding: const EdgeInsets.symmetric(vertical: 120),
+          title: Text(invoice.invoiceNumber),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Kontrahent:'),
+              Text(
+                invoice.counterpartyName,
+                style: const TextStyle(fontSize: 22.0),
+              ),
+              const SizedBox(height: 16),
+              const Text('Kwota netto:'),
+              Text(
+                '${invoice.netAmount.toString()} zł',
+                style: const TextStyle(fontSize: 22.0),
+              ),
+              const SizedBox(height: 16),
+              Text('Vat ${(invoice.vat * 100).round()} %'),
+              Text(
+                '${(invoice.netAmount * invoice.vat).toStringAsFixed(2)} zł',
+                style: const TextStyle(fontSize: 22.0),
+              ),
+              const SizedBox(height: 16),
+              const Text('Kwota brutto:'),
+              Text(
+                '${(invoice.netAmount * invoice.vat + invoice.netAmount).toStringAsFixed(2)} zł',
+                style: const TextStyle(fontSize: 22.0),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Zamknij'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
